9e84053198d554fa51f70048a10c85b037d113ba
zuhraazzahra658 <zuhraazzahra658@gmail.com>
2025-09-25 11:11:24 +0700

    setup awal

---
+++ tugas.php
@@ -0,0 +1,13 @@
+<?php
+// Daftar barang & harga dipisah
+$barang = ["teh pucuk", "coca cola", "mizeone", "aqua", "le mineral"];
+$harga  = [12000, 18000, 13000, 10000, 15000];
+
+echo "<h1>--POLGAN MART--</h1>";
+
+// Array untuk simpan hasil belanja
+$beli = [];
+$jumlah = [];
+$total = [];
+$grandtotal = 0;
+
